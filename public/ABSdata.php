196909843a4c473ccf9927b733d24f07835b8389
Andrew Ridgway <armistace@riseup.net>
2015-06-30 13:12:08 +1000

    this refs and almost #15 #17 #18 - class works probably could build a function for each of my datasetID's

--- public/ABSdata.php
+++ public/ABSdata.php
@@ -1,24 +1,40 @@
 <?php
+    require(__DIR__ . "/../includes/config.php");
+
 	if($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET["SA2"]))
-	{
-		$sa2 = $_GET["SA2"];
-		$url = "http://stat.abs.gov.au/itt/query.jsp?method=GetGenericData&datasetid=ABS_ANNUAL_ERP_ASGS&and=FREQUENCY.A,MEASURE.ERP,ASGS_2011.$sa2,REGIONTYPE.SA2&&start=2014&format=json";
-		
-		$JSON = file_get_contents($url);
-		
-		header("Content-type: application/json");
-		print($JSON);
+    {
+        //use ABS class to set values
+        $data= new ABS;
+        $data->dataSetId = "ABS_ANNUAL_ERP_ASGS";
+        $data->concepts=array("FREQUENCY","MEASURE","ASGS_2011","REGIONTYPE");
+        $data->conceptCodes=array("A","ERP",$_GET["SA2"],"SA2");
+
+        //Build the URL
+        $data->getDataURL();
+
+        //grab the JSON
+        $data->loadJSON();
+
+        //serve the JSON
+        $data->serveJSON();
 		
 	}
 	else if($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET["SA3"]))
-	{
-		$sa3 = $_GET["SA3"];
-		$url = "http://stat.abs.gov.au/itt/query.jsp?method=GetGenericData&datasetid=ABS_ANNUAL_ERP_ASGS&and=FREQUENCY.A,MEASURE.ERP,ASGS_2011.$sa3,REGIONTYPE.SA3&&start=2014&format=json";
-		
-		$JSON = file_get_contents($url);
-		
-		header("Content-type: application/json");
-		print($JSON);
+    {
+        //use ABS class to set values
+        $data= new ABS;
+        $data->dataSetId = "ABS_ANNUAL_ERP_ASGS";
+        $data->concepts=array("FREQUENCY","MEASURE","ASGS_2011","REGIONTYPE");
+        $data->conceptCodes=array("A","ERP",$_GET["SA3"],"SA3");
+
+        //Build the URL
+        $data->getDataURL();
+
+        //grab the JSON
+        $data->loadJSON();
+
+        //serve the JSON
+        $data->serveJSON();
     }
     
     
